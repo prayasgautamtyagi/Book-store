7d054fd75e1a81c325d61fea594d60d674a47b4f
prayas <prayas5gautam@gmail,com>
2018-02-16 00:46:46 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { BookDetailComponent } from './pages/book-detail/book-detail.component';
+import { HttpInterceptorService } from './core/http-interceptor.service';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    CommonModule,
+    AppRoutingRoutingModule
   ],
-  providers: [],
+  providers : [HttpInterceptorService, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
